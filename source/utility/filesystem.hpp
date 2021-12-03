cc19756631850c74cd10ede4bc526c8262e02ac6
JonathanCline <59320680+JonathanCline@users.noreply.github.com>
2021-12-03 02:17:49 -0600

    Dev/httplib (#1)
    * Added http lib support
    
    * Added basic structure for oath and filehandling
    
    * Added env handling

---
+++ source/utility/filesystem.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <filesystem>
+
+namespace lbx::fs
+{
+	using namespace std::filesystem;
+};
--- source/utility/json.hpp
+++ source/utility/json.hpp
@@ -1,8 +1,19 @@
 #pragma once
 
+#include "filesystem.hpp"
+
 #include <nlohmann/json.hpp>
 
 namespace lbx
 {
-    using json = nlohmann::json;
+	using json = nlohmann::json;
+	using json_exception = json::exception;
+
+	/**
+	 * @brief Reads a json file into memory
+	 * @param _filepath Path to the file path
+	 * @return Json object
+	 * @exception json_exception Thrown by nlohmann json on failure to parse
+	*/
+	json read_json_file(const fs::path& _filepath);
 };
